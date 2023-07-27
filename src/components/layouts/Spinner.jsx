078de496151e357ce4c78d31ddf1220584cba2d2
nikhilpadiyath <nikhilpadiyath@gmail.com>
2023-07-27 09:08:46 +0530

    Added singel user details

--- src/components/layouts/Spinner.jsx
+++ src/components/layouts/Spinner.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import spinner from '../assets/Spinner-1s-200px.gif'
+import spinner from './assets/Spinner-1s-200px.gif'
 
 function Spinner() {
   return <img src={spinner} 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import UserResults from '../components/users/UserResults'
+import UserSearching from '../components/users/UserSearching'
 
 function Home() {
   return (
     <>
+    <UserSearching />
     <UserResults />
     </>
   )
